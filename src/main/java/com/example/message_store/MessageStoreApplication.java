91be8b6d7a6dff36c6c89d72f9e620dcd53c770c
Alina Astapovich <alina.astapovich@electrolux.com>
2025-08-31 13:01:28 +0200

    init commit

---
+++ src/main/java/com/example/message_store/MessageStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.message_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/message_store/MessageStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.message_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
